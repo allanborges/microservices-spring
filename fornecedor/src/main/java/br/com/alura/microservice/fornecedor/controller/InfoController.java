89ff7f5cf1a9397aef32cde0d9b584081922cc08
Allan Borges <allanborges@gmail.com>
2020-02-18 22:57:55 -0300

    - projetos

---
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -0,0 +1,24 @@
+package br.com.alura.microservice.fornecedor.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.microservice.fornecedor.model.InfoFornecedor;
+import br.com.alura.microservice.fornecedor.services.InfoService;
+
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+	
+	@Autowired
+	private InfoService infoService;
+	
+	@RequestMapping("/{estado}")
+	public InfoFornecedor getInfoPoEstado(@PathVariable String estado) {
+		return infoService.getInfoPorEstado(estado);
+	}
+	
+
+}
